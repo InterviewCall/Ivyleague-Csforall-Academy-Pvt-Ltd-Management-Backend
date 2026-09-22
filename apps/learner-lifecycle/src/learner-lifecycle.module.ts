ceddfb5a280394bedc7195dac2ee9dbed28ff847
Madhuboni Bhattacharya <madhubonibhattacharya03@gmail.com>
2026-09-22 16:40:01 +0530

    saved

--- apps/learner-lifecycle/src/learner-lifecycle.module.ts
+++ apps/learner-lifecycle/src/learner-lifecycle.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+
+import { CommonModule } from '@app/common';
+import { ModelModule } from '@app/model';
+import { RbacModule, RolesGuard } from '@app/rbac';
+
 import { LearnerLifecycleController } from './learner-lifecycle.controller.js';
 import { LearnerLifecycleService } from './learner-lifecycle.service.js';
 
 @Module({
-  imports: [],
-  controllers: [LearnerLifecycleController],
-  providers: [LearnerLifecycleService],
+    imports: [
+        RbacModule.register(),
+        CommonModule,
+        ModelModule,
+    ],
+    controllers: [LearnerLifecycleController],
+    providers: [
+        LearnerLifecycleService,
+        { provide: APP_GUARD, useExisting: RolesGuard },
+    ],
 })
 export class LearnerLifecycleModule {}
